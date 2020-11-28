72a81b1f41b8724c80d5b7c6f4f2d2859bc66919
Bryn-ITDevAMP <brian@airmasgroup.co.id>
2020-11-28 22:38:13 +0700

    Minor update Splashscreen

--- App/Pages/Splash.js
+++ App/Pages/Splash.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import {
   ActivityIndicator,
+  Dimensions,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import * as Styles from 'App/Styles';
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -18,14 +22,27 @@ class Splash extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.props.navigation.navigate('Home');
-    }, 3500);
+    }, 800);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Splash Screen</Text>
-        <ActivityIndicator />
+        <Image
+          fadeDuration={0}
+          source={Styles.Images.app_logo}
+          style={{
+            position: 'absolute',
+            resizeMode: 'contain',
+            width: width / 2,
+            height: height / 2,
+          }}
+        />
+        <View style={{
+          marginTop: width * 0.5
+        }}>
+          <ActivityIndicator />
+        </View>
       </View>
     );
   }
