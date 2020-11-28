4c3cb8c806c6ba684d0c47489c7cdd94b8c38ab1
Bryn-ITDevAMP <brian@airmasgroup.co.id>
2020-11-28 22:34:36 +0700

    Add new assets icon and register to Images.js

--- App/Styles/Images.js
+++ App/Styles/Images.js
@@ -2,6 +2,14 @@
  * Images should be stored in the `App/Assets/Images` directory and referenced using variables defined here.
  */
 
+const IMAGES_PATH = 'App/Assets/Images/';
+const ICONS_PATH = 'App/Assets/Icons/';
+
 export default {
-  user_default: require('App/Assets/Images/user_default.jpg'),
+  app_logo: require(IMAGES_PATH + 'spacestock.png'),
+  user_default: require(IMAGES_PATH + 'user_default.jpg'),
+  exp_service_1: require(IMAGES_PATH + 'exp-service-1.png'),
+  exp_service_2: require(IMAGES_PATH + 'exp-service-2.png'),
+  exp_service_3: require(IMAGES_PATH + 'exp-service-3.png'),
+  exp_service_5: require(IMAGES_PATH + 'exp-service-5.png'),
 }
