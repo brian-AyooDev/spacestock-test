b03e6832b6abef237b8eabc432ccfff76ea3fa96
Bryn-ITDevAMP <brian@airmasgroup.co.id>
2020-11-28 22:35:38 +0700

    Update App.js, initialize setGLobalProps

--- App/App.js
+++ App/App.js
@@ -3,12 +3,36 @@ import configureStore from 'App/Components/Redux/Store';
 import { Provider } from 'react-redux';
 import AppNavigator from 'App/Navigations/Navigator';
 import { Root } from 'native-base';
+import {
+  setCustomText,
+  setCustomTextInput,
+  setCustomTouchableOpacity,
+} from 'react-native-global-props';
+import * as Styles from 'App/Styles';
 
 export default class App extends Component {
   constructor() {
     super();
     // Create Store Redux
     this.store = configureStore({});
+    this.setGlobalProps();
+  }
+
+  setGlobalProps() {
+    const customProps = {
+      allowFontScaling: false,
+      style: {
+        color: Styles.Colors.black,
+      }
+    };
+
+    const customPropsTouchableOpacity = {
+      delayPressIn: 0
+    };
+
+    setCustomText(customProps);
+    setCustomTextInput(customProps);
+    setCustomTouchableOpacity(customPropsTouchableOpacity);
   }
 
   render() {
