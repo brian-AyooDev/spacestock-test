9683b285f493471c34cc6ce8e429c3e91f3a2574
Bryn-ITDevAMP <brian@airmasgroup.co.id>
2020-11-28 22:33:43 +0700

    Store assets dummy JSON file data

---
+++ App/Assets/DummyData/index.js
@@ -0,0 +1,13 @@
+export default {
+  'App/Assets/DummyData/complex/buy/Jakarta_Pusat/data.json': require('App/Assets/DummyData/complex/buy/Jakarta_Pusat/data.json'),
+  'App/Assets/DummyData/complex/buy/Jakarta_Selatan/data.json': require('App/Assets/DummyData/complex/buy/Jakarta_Selatan/data.json'),
+  'App/Assets/DummyData/complex/rent/Jakarta_Pusat/data.json': require('App/Assets/DummyData/complex/rent/Jakarta_Pusat/data.json'),
+  'App/Assets/DummyData/complex/rent/Jakarta_Selatan/data.json': require('App/Assets/DummyData/complex/rent/Jakarta_Selatan/data.json'),
+  'App/Assets/DummyData/tower/area/Jakarta_Pusat/data.json': require('App/Assets/DummyData/tower/area/Jakarta_Pusat/data.json'),
+  'App/Assets/DummyData/tower/area/Jakarta_Selatan/data.json': require('App/Assets/DummyData/tower/area/Jakarta_Selatan/data.json'),
+  'dataComplexBuy': require('App/Assets/DummyData/homepage/complex-buy.json'),
+  'dataComplexRent': require('App/Assets/DummyData/homepage/complex-rent.json'),
+  'dataCoworkingSpace': require('App/Assets/DummyData/homepage/unit.json'),
+  'dataDiscover': require('App/Assets/DummyData/homepage/area.json'),
+  'dataOfficeTower': require('App/Assets/DummyData/homepage/tower.json'),
+}
